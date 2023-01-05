0e2d5417de772f5d9e7fb81ad8e6cfbe1b95c7d1
tekyinblack <67026054+tekyinblack@users.noreply.github.com>
2023-01-05 10:47:13 +0000

    Add files via upload

---
+++ PS3_Car_Tank_v1.ino
@@ -0,0 +1,154 @@
+/* Code to be used with an ESP32 D1 controller board, similar layout to an Arduino Uno
+ * alongside a single L298D motor driver board. The robot has four wheels and a motor
+ * for each wheel, the L298D is connected to and driving both wheels on a side in parallel.
+ * This code drives the platform as a tank, with stop and turn steering. 
+*/
+
+#include <Ps3Controller.h>
+
+//Right motor pin definitions
+int enableRightMotor = 14;
+int rightMotorPin1 = 16;
+int rightMotorPin2 = 27;
+//Left motor pin definitions
+int enableLeftMotor = 26;
+int leftMotorPin1 = 25;
+int leftMotorPin2 = 17;
+
+#define MAX_MOTOR_SPEED 255
+#define THRESHOLD 25
+
+long oldLeftValue = 0;
+long oldRightValue = 0;
+
+int connectedLED = 0;
+
+const int PWMFreq = 1000; /* 1 KHz */
+const int PWMResolution = 8;
+const int PWMSpeedChannel = 4;
+const int rightMotorPWMSpeedChannel = 4;
+const int leftMotorPWMSpeedChannel = 5;
+
+// vary the power applied to prevent on/off working of the motors
+long applyCurve(long joystick) {
+  if (joystick > 0) {
+    return (MAX_MOTOR_SPEED - THRESHOLD) * map(pow(joystick, 3), -2097152, 2048383, -MAX_MOTOR_SPEED, MAX_MOTOR_SPEED)/MAX_MOTOR_SPEED + THRESHOLD;
+  } else if (joystick < 0) {
+    return (MAX_MOTOR_SPEED - THRESHOLD) * map(pow(joystick, 3), -2097152, 2048383, -MAX_MOTOR_SPEED, MAX_MOTOR_SPEED)/MAX_MOTOR_SPEED - THRESHOLD;
+  }
+  return 0;
+}
+
+// accomodate PS3 controllers with imperfect rest positions
+long deadZone(long joystick) {
+  if (abs(joystick) < THRESHOLD) {
+    return 0;
+  }
+  return joystick;
+}
+
+// handle the input from the PS3 controllers
+void notify() {
+  long leftValue = (Ps3.data.analog.stick.ly);   //Left stick  - y axis - forward/backward car movement
+  long rightValue = (Ps3.data.analog.stick.ry);  //Right stick - x axis - left/right car movement
+
+  if (oldLeftValue == leftValue && oldRightValue == rightValue) 
+   {
+     return;
+   }
+  oldLeftValue = leftValue;
+  oldRightValue = rightValue;
+
+  leftValue = deadZone(leftValue);
+  rightValue = deadZone(rightValue);
+
+  leftValue = applyCurve(leftValue);
+  rightValue = applyCurve(rightValue);
+
+
+  Serial.println("left:" + String(leftValue) + " right:" + String(rightValue));
+
+  rotateMotor(rightValue, leftValue);
+}
+
+
+// on PS3 connected, light LED
+void onConnect() {
+  Serial.println("Connected!.");
+  connectedLED = 1;
+}
+// on PS3 disconnected, extinguish LED
+void onDisConnect() {
+  rotateMotor(0, 0);
+  connectedLED = 0;
+}
+
+// apply motor power
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed) {
+  if (rightMotorSpeed > 0) {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
+  } else if (rightMotorSpeed < 0) {
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
+  } else {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, LOW);
+  }
+
+  if (leftMotorSpeed > 0) {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+  } else if (leftMotorSpeed < 0) {
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
+  } else {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, LOW);
+  }
+  ledcWrite(rightMotorPWMSpeedChannel, abs(rightMotorSpeed));
+  ledcWrite(leftMotorPWMSpeedChannel, abs(leftMotorSpeed));
+}
+
+// setup routine for pins
+void setUpPinModes() {
+  pinMode(enableRightMotor, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
+
+  pinMode(enableLeftMotor, OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+
+  //Set up PWM for motor speed
+  ledcSetup(rightMotorPWMSpeedChannel, PWMFreq, PWMResolution);
+  ledcSetup(leftMotorPWMSpeedChannel, PWMFreq, PWMResolution);
+  ledcAttachPin(enableRightMotor, rightMotorPWMSpeedChannel);
+  ledcAttachPin(enableLeftMotor, leftMotorPWMSpeedChannel);
+
+  rotateMotor(0, 0);
+}
+
+//  Main setup routine, contains the PS3 address which must be updated
+void setup() {
+  setUpPinModes();
+  Serial.begin(115200);
+  Serial.println(String(__FILE__) + " " + String(__DATE__) + " " + String(__TIME__));
+  Ps3.attach(notify);
+  Ps3.attachOnConnect(onConnect);
+  Ps3.attachOnDisconnect(onDisConnect);
+  Ps3.begin("A0:5A:5A:A0:09:84");  // <---- Update this address for the PS3 in use
+  Serial.println("Ready.");
+}
+
+// main loop flashes LED to indicate PS3 connection
+void loop() {
+  if (connectedLED) {
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
